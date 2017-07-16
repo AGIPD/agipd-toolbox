44096936edd685bf73cfd751de5345a7dbcbb28b
Yaroslav Gevorkov <yaroslav.gevorkov@desy.de>
2017-07-16 04:21:52 +0200

    documentation

--- agipdCalibration/algorithms/helperFunctions.py
+++ agipdCalibration/algorithms/helperFunctions.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
+# fast course order filter. Instead of taking 2x windowRadius values, take only lowpassSamplePointsCount and thus speed up the computation
 def orderFilterCourse(data, windowRadius, order, lowpassSamplePointsCount):
     if data.ndim != 1:
         raise ValueError('data must be one dimensional')
 
-    if windowRadius%2 == 0:
+    if windowRadius % 2 == 0:
         windowRadius -= 1
 
     samplePositions = np.round(np.linspace(windowRadius, data.size - windowRadius, lowpassSamplePointsCount)).astype(int)
@@ -14,5 +15,5 @@ def orderFilterCourse(data, windowRadius, order, lowpassSamplePointsCount):
         neighbourhood = data[(samplePositions[i] - windowRadius):(samplePositions[i] + windowRadius + 1)]
         sampleValues[i] = np.sort(neighbourhood)[order]
 
-    lowPassData = np.interp(np.arange(data.size), samplePositions, sampleValues, left = sampleValues[0], right = sampleValues[-1])
+    lowPassData = np.interp(np.arange(data.size), samplePositions, sampleValues, left=sampleValues[0], right=sampleValues[-1])
     return lowPassData
