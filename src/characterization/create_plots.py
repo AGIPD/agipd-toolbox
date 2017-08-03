79eada168c3fd0337808e196a2bd277cab98b704
Manuela Kuhn <manuela.kuhn@desy.de>
2017-08-03 16:23:14 +0200

    Added plotting script

---
+++ src/characterization/create_plots.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+from plotting import GeneratePlots
+
+def condition_function(error_code):
+    indices = np.where(error_code != 0)
+
+    return indices
+
+
+def create_dir(directory_name):
+    if not os.path.exists(directory_name):
+        try:
+            os.makedirs(directory_name)
+            print("Dir '{0}' does not exist. Create it.".format(directory_name))
+        except IOError:
+            if os.path.isdir(directory_name):
+                pass
+
+if __name__ == "__main__":
+
+    base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/processed/"
+
+    asic = 9
+    module = "M314"
+    temperature = "temperature_m15C"
+    current = "itestc150"
+
+    plot_subdir = "asic{}_failed".format(str(asic).zfill(2))
+    #plot_subdir = "manu_test"
+
+    n_processes = 10
+
+    gather_fname = os.path.join(base_dir, module, temperature, "drscs", current, "gather",
+                                "{}_drscs_{}_asic{}.h5".format(module, current,
+                                                                  str(asic).zfill(2)))
+    process_fname = os.path.join(base_dir, module, temperature, "drscs", current, "process",
+                                  "{}_drscs_{}_asic{}_processed.h5".format(module, current,
+                                                                           str(asic).zfill(2)))
+    plot_dir = os.path.join(base_dir, module, temperature, "drscs", "plots", current, plot_subdir)
+    plot_prefix = "{}_{}".format(module, current)
+
+    create_dir(plot_dir)
+
+    obj = GeneratePlots(asic, gather_fname, plot_prefix, plot_dir, n_processes)
+
+    #idx = (3,60,1)
+    #idx = (62, 6, 1)
+    #obj.run_idx(idx)
+
+    obj.run_condition(process_fname, condition_function)
--- src/characterization/plotting.py
+++ src/characterization/plotting.py
@@ -1,5 +1,8 @@
+import os
 import matplotlib.pyplot as plt
 import numpy as np
+import h5py
+from multiprocessing import Pool
 
 
 def gerenate_data_hist(current_idx, scaled_x_values, digital,
@@ -195,3 +198,133 @@ def generate_all_plots(current_idx, scaled_x_values, analog,
                            plot_title,
                            plot_name)
 
+def generate_idx_plot(plot_file_prefix, plot_title_prefix, plot_ending, plot_idx,
+                      analog, digital, x_values):
+    idx = plot_idx + (slice(None),)
+
+    plot_file_prefix = "{}_[{}, {}]_{}".format(plot_file_prefix,
+                                               idx[0],
+                                               idx[1],
+                                               str(idx[2]).zfill(3))
+    plot_title_prefix = "{}_[{}, {}]_{}".format(plot_title_prefix,
+                                                idx[0],
+                                                idx[1],
+                                                str(idx[2]).zfill(3))
+    plot_title = "{} data".format(plot_title_prefix)
+    plot_name = "{}_data{}".format(plot_file_prefix,
+                                   plot_ending)
+
+    print("generate plot")
+    generate_data_plot(idx,
+                       x_values,
+                       analog,
+                       digital,
+                       plot_title,
+                       plot_name)
+
+class GeneratePlots():
+    def __init__(self, asic, input_fname, plot_prefix, plot_dir, n_processes):
+
+        self.asic = asic
+        self.input_fname = input_fname
+        self.process_fname = None
+        self.plot_dir = plot_dir
+
+        print("plot_prefix", plot_prefix)
+        self.plot_file_prefix = "{}_asic{}".format(plot_prefix, str(asic).zfill(2))
+        self.plot_file_prefix = os.path.join(self.plot_dir, self.plot_file_prefix)
+
+        self.plot_title_prefix = self.plot_file_prefix.rsplit("/", 1)[1]
+        print("plot_file_prefix", self.plot_file_prefix)
+        print("plot_title_prefix", self.plot_title_prefix)
+
+        self.plot_ending = ".png"
+        self.create_plot_type = None
+
+        self.scaling_point = 200
+        self.scaling_factor = 10
+
+        self.n_processes = n_processes
+
+    def load_process_results(self):
+        process_file = h5py.File(self.process_fname, "r")
+
+        self.error_code = process_file["/error_code"][()]
+
+        process_file.close()
+
+    def load_raw_data(self, idx=None):
+        source_file = h5py.File(self.input_fname, "r")
+
+        if idx is not None:
+            analog = source_file["/entry/instrument/detector/data"][idx]
+            digital = source_file["/entry/instrument/detector/data_digital"][idx]
+        else:
+            analog = source_file["/entry/instrument/detector/data"][()]
+            digital = source_file["/entry/instrument/detector/data_digital"][()]
+        source_file.close()
+
+        return analog, digital
+
+    def scale_full_x_axis(self, s):
+        number_of_x_values = s
+
+        lower  = np.arange(self.scaling_point)
+        upper = (np.arange(self.scaling_point, number_of_x_values)
+                 * self.scaling_factor
+                 - self.scaling_point * self.scaling_factor
+                 + self.scaling_point)
+
+        scaled_x_values = np.concatenate((lower, upper))
+
+        return scaled_x_values
+
+    def run_idx(self, idx):
+        analog, digital = self.load_raw_data(idx)
+        number_of_x_values = analog.shape[0]
+
+        scaled_x_values = self.scale_full_x_axis(number_of_x_values)
+
+        generate_idx_plot(self.plot_file_prefix, self.plot_title_prefix,
+                          self.plot_ending, idx, analog, digital,
+                          scaled_x_values)
+
+    def run_condition(self, process_fname, condition_function):
+        self.process_fname = process_fname
+        self.load_process_results()
+
+        # start 4 worker processes
+        pool = Pool(processes=self.n_processes)
+        result_list = []
+
+        plot_indices = condition_function(self.error_code)
+
+        if plot_indices[0].size != 0:
+            idx = (plot_indices[0][0], plot_indices[1][0], plot_indices[2][0])
+            analog, digital = self.load_raw_data(idx)
+            number_of_x_values = analog.shape[0]
+
+            scaled_x_values = self.scale_full_x_axis(number_of_x_values)
+
+        for i in np.arange(len(plot_indices[0])):
+            idx = (plot_indices[0][i], plot_indices[1][i], plot_indices[2][i])
+            print('idx', idx)
+
+            analog, digital = self.load_raw_data(idx)
+
+            if analog.shape[0] != number_of_x_values:
+                scaled_x_values = self.scale_full_x_axis(analog.shape[0])
+
+            result_list.append(
+                pool.apply_async(
+                    generate_idx_plot,
+                    (self.plot_file_prefix,
+                     self.plot_title_prefix,
+                     self.plot_ending,
+                     idx,
+                     analog,
+                     digital,
+                     scaled_x_values)))
+
+            for process_result in result_list:
+                process_result.get()
