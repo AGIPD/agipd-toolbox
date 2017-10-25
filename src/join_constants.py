bc6b21d25f52913c5856f978a8c6c8bbe72e3bf8
Manuela Kuhn <manuela.kuhn@desy.de>
2017-10-25 14:39:01 +0200

    Added first unfinished version of join constants

---
+++ src/join_constants.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import time
+import numpy as np
+import h5py
+
+import utils
+
+
+class JoinConstants():
+    def __init__(self):
+        self.base_dir = "/home/kuhnm/ownCloud/calibration_data_example"
+        self.input_fname = "Pedestal_in_ADU_m3_P11burst1k_nofit.hdf5"
+
+        self.source_content = None
+
+        self.run()
+
+    def run(self):
+
+        fname = "/home/kuhnm/tmp/test/test.h5"
+
+        f = None
+        try:
+            f = h5py.File(fname, "w")
+
+            for channel in range(16):
+                fname = os.path.join(self.base_dir, self.input_fname)
+
+                print("loading content of file {}".format(fname))
+                file_content = utils.load_file_content(fname)
+
+                prefix = "channel{:02d}".format(channel)
+                for key in file_content:
+                    f.create_dataset(prefix + "/"+ key, data=file_content[key])
+
+                f.flush()
+        finally:
+            if f is not None:
+                f.close()
+
+if __name__ == "__main__":
+    JoinConstants()
