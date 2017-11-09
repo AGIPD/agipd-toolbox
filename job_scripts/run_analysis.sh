2a6e9d4bd267584104649b4253228dbb88b5bc15
Manuela Kuhn <manuela.kuhn@desy.de>
2017-11-09 16:13:33 +0100

    Convert data format instead of recalculate results in run_analysis

--- job_scripts/run_analysis.sh
+++ job_scripts/run_analysis.sh
@@ -5,49 +5,48 @@ git_dir=/home/kuhnm/calibration
 
 raw_dir=/gpfs/exfel/exp/SPB/201730/p900009
 output_dir=/gpfs/exfel/exp/SPB/201730/p900009/scratch/user/kuhnm
-channel="0"
-#channel="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
+#channel="0"
+channel="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
 
-meas_type=dark
-run_list="428 429 430"
+#meas_type=dark
+#run_list="428 429 430"
 
-#meas_type=pcdrs
-#run_list="488 489 490 491 492 493 494 495"
-
-date_id=2017-10-27 # e.g. from here: dark_AGIPD{:02d}_agipd_2017-10-27.h5
+meas_type=pcdrs
+run_list="488 489 490 491 492 493 494 495"
 
 #####
 
-
 source /etc/profile.d/modules.sh; module load anaconda/3; module load texlive/2017; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-anaconda/py3-4.3.0
 
-for ch in $channel
+#output_dir=${output_dir}/${run_list// /-}
+
+for ch in ${channel}
 do
+    printf "\nWorking on channel $ch\n"
     if [ "$meas_type" == "pcdrs" ]
     then
-        echo $ch
         python ${git_dir}/job_scripts/start_analyse.py --input_dir $raw_dir --output_dir $output_dir --channel $ch --run_type gather --type $meas_type --run_list ${run_list} --use_xfel_in_format
 
     elif  [ "$meas_type" == "dark" ]
     then
         for run in $run_list
         do
-            echo $ch $run
+            printf "run $run"
             python ${git_dir}/job_scripts/start_analyse.py --input_dir $raw_dir --output_dir $output_dir --channel $ch --run_type gather --type $meas_type --run_list $run --use_xfel_in_format
         done
 
     fi
 
-     echo $ch
-     # generates constants in the agipd format
-     python ${git_dir}/job_scripts/start_analyse.py --input_dir $output_dir --output_dir $output_dir --channel $ch --run_type process --type $meas_type --run_list ${run_list} --use_xfel_in_format
+    # generates constants in the agipd format
+    python ${git_dir}/job_scripts/start_analyse.py --input_dir $output_dir --output_dir $output_dir --channel $ch --run_type process --type $meas_type --run_list ${run_list} --use_xfel_in_format
 
-     # generates constants in the xfel format
-    python ${git_dir}/job_scripts/start_analyse.py --input_dir $output_dir --output_dir $output_dir --channel $ch --run_type process --type $meas_type --run_list ${run_list} --use_xfel_in_format --use_xfel_out_format
+    # generates constants in the xfel format
+    python src/convert_format.py --base_dir ${output_dir}/${meas_type} --input_file ${meas_type}_AGIPD{:02d}_agipd.h5 --output_file ${meas_type}_AGIPD{:02d}_{}.h5 --channel ${ch} --output_format xfel
+#    python ${git_dir}/job_scripts/start_analyse.py --input_dir $output_dir --output_dir $output_dir --channel $ch --run_type process --type $meas_type --run_list ${run_list} --use_xfel_in_format --use_xfel_out_format
 done
 
 # joins constants in the agipd format
-python ${git_dir}/src/join_constants.py --base_dir $output_dir/$meas_type --input_file ${meas_type}_AGIPD{:02d}_agipd_${date_id}.h5 --output_file ${meas_type}_joint_constants_agipd.h5
+python ${git_dir}/src/join_constants.py --base_dir $output_dir/$meas_type --input_file ${meas_type}_AGIPD{:02d}_agipd.h5 --output_file ${meas_type}_joint_constants_agipd.h5
 
 # joins constants in the xfel format
-python ${git_dir}/src/join_constants.py --base_dir $output_dir/$meas_type --input_file ${meas_type}_AGIPD{:02d}_xfel_${date_id}.h5 --output_file ${meas_type}_joint_constants_xfel.h5
+python ${git_dir}/src/join_constants.py --base_dir $output_dir/$meas_type --input_file ${meas_type}_AGIPD{:02d}_xfel.h5 --output_file ${meas_type}_joint_constants_xfel.h5
---
+++ src/convert_format.py
@@ -0,0 +1,105 @@
+import os
+import argparse
+import h5py
+
+import utils
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--base_dir",
+                        type=str,
+                        required=True,
+                        help="Base directory to work in")
+    parser.add_argument("--input_file",
+                        type=str,
+                        required=True,
+                        help="Name of the input files")
+    parser.add_argument("--output_file",
+                        type=str,
+                        required=True,
+                        help="Template of the output file")
+    parser.add_argument("--channel",
+                        type=int,
+                        required=True,
+                        help="Module to gather, e.g 1 (XFEL)")
+    parser.add_argument("--output_format",
+                        type=str,
+                        required=True,
+                        help="Format to convert the data to (xfel or agipd)")
+
+    args = parser.parse_args()
+
+    return args
+
+
+class ConvertFormat():
+    def __init__(self, input_fname, output_fname, output_format, channel, key_list=None):
+        self.input_fname = input_fname
+        self.output_fname = output_fname
+        self.output_format = output_format
+        self.channel = channel
+
+        if key_list is None:
+            f = None
+            try:
+                f = h5py.File(self.input_fname, "r")
+
+                key_list = list(f.keys())
+            finally:
+                if f is not None:
+                    f.close()
+
+            key_list.remove("collection")
+            self.keys_to_convert = key_list
+
+        else:
+            self.keys_to_convert = key_list
+        print("Keys to convert: {}\n".format(self.keys_to_convert))
+
+    def run(self):
+        print("Loading input_file from {}".format(self.input_fname))
+        file_content = utils.load_file_content(input_fname)
+
+        print("Converting")
+        for key in self.keys_to_convert:
+            file_content[key] = self.convert(file_content[key])
+
+        print("Writing output_file to {}".format(self.output_fname))
+        utils.write_content(self.output_fname, file_content)
+
+    def convert(self, data):
+        if self.output_format == "xfel":
+            return utils.convert_to_xfel_format(self.channel, data)
+        elif self.output_format == "agipd":
+            return utils.convert_to_agipd_format(self.channel, data)
+        else:
+            msg = "Format to which data should be converted is not supported."
+            raise Exception(msg)
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+
+    base_dir = args.base_dir
+    input_filename = args.input_file
+    output_filename = args.output_file
+    channel = args.channel
+    output_format = args.output_format
+
+
+#    base_dir = "/gpfs/exfel/exp/SPB/201730/p900009/scratch/user/kuhnm/pcdrs"
+#    channel = 0
+#
+#    input_filename = "pcdrs_AGIPD{:02d}_agipd.h5"
+    input_fname = os.path.join(base_dir, input_filename.format(channel))
+#
+#    #output_format = "agipd"
+#    output_format = "xfel"
+#
+    output_filename = "pcdrs_AGIPD{:02d}_{}.h5"
+    output_fname = os.path.join(base_dir,
+                                output_filename.format(channel, output_format))
+
+    obj = ConvertFormat(input_fname, output_fname, output_format, channel)
+    obj.run()
