5134a6e872370f8f9dcd5340be2741efeb040647
Manuela Kuhn <manuela.kuhn@desy.de>
2017-11-16 10:48:49 +0100

    Fixed module depenency to latest

--- job_scripts/start_analyse.sh
+++ job_scripts/start_analyse.sh
@@ -4,7 +4,8 @@
 #source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh
 #module load cfel-anaconda/py3-4.3.0
 
-source /etc/profile.d/modules.sh; module load anaconda/3; module load texlive/2017; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-anaconda/py3-4.3.0
+#source /etc/profile.d/modules.sh; module load anaconda/3; module load texlive/2017; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-anaconda/py3-4.3.0
+source /etc/profile.d/modules.sh; module load anaconda/3; module load texlive/2017; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-python3/latest
 
 
 SCRIPT_DIR=$1
