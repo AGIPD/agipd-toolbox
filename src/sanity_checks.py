60f29e99348ba99a949150db4110ebe49a72d2b5
Manuela Kuhn <manuela.kuhn@desy.de>
2017-12-08 10:26:32 +0100

    Sanity Checks: Added test for * detect trailing zeros in trainId and data * detect (not trailing) zeros * test monotonity of trainId * detect train loss * compare data and trainId dimension

--- src/sanity_checks.py
+++ src/sanity_checks.py
@@ -36,9 +36,17 @@ def create_epilog():
                                 "modules"),
         "test_train_id_equal": ("Checks if the train ids taken from detector, header\n"
                                 "and trailer are equal (per module)"),
-        "test_dim_data": ("Checks if the sum of the pulseCount entries is\n"
-                          "corresponding to the data"),
-        "test_diff_train_id": ("Checks if the trainId is monotonically increasing"),
+        "test_data_vs_pulsec": ("Checks if the sum of the pulseCount entries is\n"
+                                "corresponding to the data"),
+        "test_train_id_diff":  ("Checks if the trainId is monotonically increasing"),
+        "test_train_id_tzero": ("Checks number of placeholder in trainId and if they are\n"
+                                "always at the end"),
+        "test_train_id_zeros": ("Checks if trainId containes zeros which are not at the\n"
+                                "end"),
+        "test_train_loss":     ("Checks for missing entries in trainId"),
+        "test_data_tzeros":    ("Check if additional data entries are trailing zeros"),
+        "test_data_vs_tr_id":  ("Checks if the dimension of the image trainId is\n"
+                                "corresponding to the data)"),
     }
 
     # determine how long the space for the keys should be
@@ -119,6 +127,38 @@ def read_in_data(file_raw_temp, channel, dict_key, read_in_path, seq_start,
             read_in_data = utils.convert_to_agipd_format(channel, read_in_data)
 
         data[channel][dict_key].append(read_in_data)
+
+
+def get_trailing_zeros(data):
+    res = []
+
+    for seq in range(len(data)):
+
+        eq_zero_idx = np.where(data[seq] == 0)[0]
+        #print(eq_zero_idx)
+        diff_zeros = np.diff(eq_zero_idx)
+        #print(diff_zeros)
+        non_consecutive_idx = np.where(diff_zeros != 1)[0]
+        #print(non_consecutive_idx)
+
+        if eq_zero_idx.size == 0:
+            res.append(0)
+            continue
+        last_eq_zero_idx = len(eq_zero_idx)
+        if last_eq_zero_idx not in non_consecutive_idx:
+            # calculate how many zeros there are
+            if non_consecutive_idx.size == 0:
+                trailing_zeros_start = eq_zero_idx[0]
+            else:
+                trailing_zeros_start = eq_zero_idx[non_consecutive_idx[-1] + 1]
+            trailing_zeros_stop = eq_zero_idx[last_eq_zero_idx - 1] + 1
+            #print(trailing_zeros_start, trailing_zeros_stop)
+            n_zeros = trailing_zeros_stop - trailing_zeros_start
+            #print("number of zeros: ", n_zeros)
+
+            res.append(n_zeros)
+
+    return res
 
 
 # for the whole module
@@ -406,7 +446,11 @@ class SanityChecks(unittest.TestCase):
                     msg = ("\nTrainIds from detector, header and trailer do not "
                             "match for channel {:02}, seq {}".format(channel, seq))
 
-    def test_dim_data(self):
+        # for clarity only print one error message for all sequences
+        if assert_failed:
+            self.fail(msg)
+
+    def test_data_vs_pulsec(self):
         """
         Checks if the sum of the pulseCount entries is corresponding to the data)
         """
@@ -437,7 +481,7 @@ class SanityChecks(unittest.TestCase):
         if assert_failed:
             self.fail(msg)
 
-    def test_diff_train_id(self):
+    def test_train_id_diff(self):
         """
         Checks if the trainId is monotonically increasing
         """
@@ -454,9 +498,200 @@ class SanityChecks(unittest.TestCase):
 
                 diff = np.diff(train_id)
 
-                msg = ("Channel {}, sequence {}: TrainId is not monotonically increasing"
-                       .format(channel, seq))
-                self.assertTrue(np.all(diff > 0), msg)
+                try:
+                    self.assertTrue(np.all(diff > 0))
+                except AssertionError:
+                    assert_failed = True
+                    msg += ("\nChannel {:02}, sequence {}"
+                            .format(channel, seq))
+
+        # for clarity only print one error message for all channels and sequences
+        if assert_failed:
+            self.fail(msg)
+
+    def test_train_id_tzero(self):
+        """
+        Checks number of placeholder in trainId and if they are always at the end
+        """
+
+        res = [[] for i in range(self._n_sequences)]
+
+        for ch, _ in enumerate(data):
+            d = data[ch]["header_train_id"]
+            seq_n_zeros = get_trailing_zeros(d)
+
+            for seq in range(self._n_sequences):
+                res[seq].append(seq_n_zeros[seq])
+
+        assert_failed = False
+        msg = ""
+        for seq, r in enumerate(res):
+            unique = np.unique(r)
+
+            try:
+                self.assertEqual(len(unique), 1)
+            except AssertionError:
+                assert_failed = True
+                msg += ("\nSequence {} has not the same number of trailing "
+                        "zeros for all channels\n({})".format(seq, r))
+
+        # for clarity only print one error message for all sequences
+        if assert_failed:
+            self.fail(msg)
+
+    def test_train_id_zeros(self):
+        """
+        Checks if trainId contains zeros which are not at the end
+        """
+
+        assert_failed = False
+        msg = ("\nTrainid contains zeros which are not at the end for following"
+               " channels and sequences:")
+        for ch, _ in enumerate(data):
+            d = data[ch]["header_train_id"]
+
+            for seq in range(len(d)):
+
+                eq_zero_idx = np.where(data[seq] == 0)[0]
+                #print(eq_zero_idx)
+                diff_zeros = np.diff(eq_zero_idx)
+                #print(diff_zeros)
+                non_consecutive_idx = np.where(diff_zeros != 1)[0]
+                #print(non_consecutive_idx)
+
+                try:
+                    self.assertEqual(non_consecutive_idx.size, 0)
+                except AssertionError:
+                    assert_failed = True
+                    msg += ("\nChannel {:02}, seq {}".format(ch, seq))
+
+        # for clarity only print one error message for all channels and sequences
+        if assert_failed:
+            self.fail(msg)
+
+    def test_train_loss(self):
+        """
+        Checks for missing entries in trainId
+        """
+
+        assert_failed = False
+        msg = ""
+        for ch, _ in enumerate(data):
+            for seq in range(self._n_sequences):
+                d = data[ch]["header_train_id"][seq]
+
+                if seq == 0:
+                    d = d[self._usable_start:]
+
+                idx_orig = np.arange(len(d))
+
+                # check trainId within a sequence
+                non_zero_idx = np.where(d != 0)
+
+                d_no_zeros = d[non_zero_idx]
+                # mapping the new indices to the original ones
+                idx_no_zeros = idx_orig[non_zero_idx]
+
+                diff = np.diff(d_no_zeros)
+                train_loss_idx = np.where(diff != 1)[0]
+
+                try:
+                    self.assertEqual(train_loss_idx.size, 0, msg)
+                except AssertionError:
+                    assert_failed = True
+                    msg += ("\nChannel {:02}, seq {}: Train loss found at indices:\n"
+                            .format(ch, seq))
+                    for idx  in train_loss_idx:
+                        idx_o = idx_no_zeros[idx]
+                        msg += ("\tidx {}: ... {} ..."
+                                .format(idx_o,
+                                        str(d[idx_o - 1:idx_o + 3])[1:-1]))
+
+                # check transition between two sequences
+                if seq != 0:
+                    try:
+                        self.assertEqual(last_seq + 1, d_no_zeros[0])
+                    except AssertionError:
+                        assert_failed = True
+                        msg = ("\nChannel {:02}: Train loss found between sequences"
+                               " {} and {}\n".format(ch, seq - 1 , seq))
+                        msg += ("(end of seq {}: {}, start of seq {}: {})"
+                                .format(seq - 1, last_seq, seq, d_no_zeros[0]))
+
+                # keep the last trainId for the next iteration
+                last_seq = d_no_zeros[-1]
+
+        # for clarity only print one error message for all channels and sequences
+        if assert_failed:
+            self.fail(msg)
+
+    def test_data_tzeros(self):
+        """
+        Check if extra data entries are trailing zeros
+        """
+
+        assert_failed = False
+        msg = "\nData containes extra data which is not zero for:"
+        for channel in range(self._n_channels):
+            for seq in range(self._n_sequences):
+                n_total_pulses = np.sum(self._data[channel]["pulse_count"][seq])
+
+                fname = self._file_raw_temp.format(channel, seq)
+                #print(fname)
+                group_name = self._path["data"].format(channel)
+
+                f = h5py.File(fname, "r")
+                data_shape = f[group_name].shape
+
+                if data_shape[0] > n_total_pulses:
+                    extra_data = f[group_name][n_total_pulses:]
+                else:
+                    extra_data = np.array([])
+
+                f.close()
+
+                try:
+                    self.assertTrue(not np.any(extra_data))
+                except AssertionError:
+                    assert_failed = True
+                    msg += ("\nChannel {:02}, seq{} (number of zeros {})"
+                           .format(channel, seq, extra_data.shape[0]))
+
+        # for clarity only print one error message for all channels and sequences
+        if assert_failed:
+            self.fail(msg)
+
+    def test_data_vs_tr_id(self):
+        """
+        Checks if the dimension of the image trainId is corresponding to the data)
+        """
+
+        assert_failed = False
+        msg = ""
+        msg = ("\nTrainId and data shape do not match for:")
+        for channel in range(self._n_channels):
+            for seq in range(self._n_sequences):
+                fname = self._file_raw_temp.format(channel, seq)
+                #print(fname)
+
+                group_name = self._path["image_train_id"].format(channel)
+                data_name = self._path["data"].format(channel)
+
+                f = h5py.File(fname, "r")
+                train_id_shape = f[group_name].shape
+                data_shape = f[data_name].shape
+                f.close()
+
+                try:
+                    self.assertEqual(train_id_shape[0], data_shape[0])
+                except AssertionError:
+                    assert_failed = True
+                    msg += ("Channel {:02}, sequence {} ({} vs {})\n"
+                            .format(channel, seq, train_id_shape[0], data_shape[0]))
+
+        # for clarity only print one error message for all channels and sequences
+        if assert_failed:
+            self.fail(msg)
 
     # per test
     def tearDown(self):
