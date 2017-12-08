9aaa56b903b3b4f634956c923e3c6d730dab847f
Manuela Kuhn <manuela.kuhn@desy.de>
2017-12-08 10:33:09 +0100

    Clean up

--- job_scripts/start_analyse.sh
+++ job_scripts/start_analyse.sh
@@ -5,7 +5,7 @@
 #module load cfel-anaconda/py3-4.3.0
 
 #source /etc/profile.d/modules.sh; module load anaconda/3; module load texlive/2017; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-anaconda/py3-4.3.0
-source /etc/profile.d/modules.sh; module load anaconda/3; module load texlive/2017; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-python3/latest
+source /etc/profile.d/modules.sh; module load anaconda/3; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-python3/latest
 
 
 SCRIPT_DIR=$1
