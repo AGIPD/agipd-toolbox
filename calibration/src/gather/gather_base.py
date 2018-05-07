b9a969ea598b8c98fccd14f066487cfa23080b0d
Manuela Kuhn <manuela.kuhn@desy.de>
2018-05-07 15:13:22 +0200

    Moved version into shared directory

--- calibration/src/gather/gather_base.py
+++ calibration/src/gather/gather_base.py
@@ -11,16 +11,22 @@ try:
 except:
     CURRENT_DIR = os.path.dirname(os.path.realpath('__file__'))
 
-BASE_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
-SRC_DIR = os.path.join(BASE_DIR, "src")
+CALIBRATION_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+SRC_DIR = os.path.join(CALIBRATION_DIR, "src")
 GATHER_DIR = os.path.join(SRC_DIR, "gather")
 LAYOUT_DIR = os.path.join(GATHER_DIR, "layouts")
+
+BASE_DIR = os.path.dirname(CALIBRATION_DIR)
+SHARED_DIR = os.path.join(BASE_DIR, "shared")
 
 if SRC_DIR not in sys.path:
     sys.path.insert(0, SRC_DIR)
 
 import utils  # noqa E402
 import cfel_optarg  # noqa E402
+
+if SHARED_DIR not in sys.path:
+    sys.path.insert(0, SHARED_DIR)
 
 from _version import __version__
 
--- calibration/src/process/process_base.py
+++ calibration/src/process/process_base.py
@@ -10,13 +10,20 @@ try:
 except:
     CURRENT_DIR = os.path.dirname(os.path.realpath('__file__'))
 
-BASE_PATH = os.path.dirname(os.path.dirname(CURRENT_DIR))
-SRC_PATH = os.path.join(BASE_PATH, "src")
+CALIBRATION_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+SRC_DIR = os.path.join(CALIBRATION_DIR, "src")
 
-if SRC_PATH not in sys.path:
-    sys.path.insert(0, SRC_PATH)
+BASE_DIR = os.path.dirname(CALIBRATION_DIR)
+SHARED_DIR = os.path.join(BASE_DIR, "shared")
+
+if SRC_DIR not in sys.path:
+    sys.path.insert(0, SRC_DIR)
 
 import utils  # noqa E402
+
+if SHARED_DIR not in sys.path:
+    sys.path.insert(0, SHARED_DIR)
+
 from _version import __version__
 
 
