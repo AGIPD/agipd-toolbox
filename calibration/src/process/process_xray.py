42583243dd6bbaf81f370f368fa1008198313588
Jennifer Poehlsen <jennifer.poehlsen@desy.de>
2018-05-17 11:39:13 +0200

    Added missing quantities: errors, peak stddev, quality

--- calibration/src/process/process_xray.py
+++ calibration/src/process/process_xray.py
@@ -1,12 +1,20 @@
-import numpy as np
+from collections import namedtuple
+
 from process_base import ProcessBase
-#import scipy
+
+import numpy as np
 from scipy import signal
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 
 class ProcessXray(ProcessBase):
+    Res = namedtuple("res", ["photon_spacing", 
+                             "spacing_error", 
+                             "peak_stddev", 
+                             "peak_error", 
+                             "quality"])
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -17,7 +25,12 @@ class ProcessXray(ProcessBase):
         self.shapes = {
             "photon_spacing": (self.n_rows,
                                self.n_cols,
-                               self.n_memcells)
+                               self.n_memcells),
+
+            "stddev":         (self.n_rows,
+                               self.n_cols,
+                               self.n_memcells,
+                               2)
         }
 
         self.result = {
@@ -29,15 +42,15 @@ class ProcessXray(ProcessBase):
             "spacing_error": {
                 "data": np.empty(self.shapes["photon_spacing"]),
                 "path": "spacing_error",
-                "type": np.int16
+                "type": np.float
             },
             "peak_stddev": {
-                "data": np.empty(self.shapes["photon_spacing"]),
+                "data": np.empty(self.shapes["stddev"]),
                 "path": "peak_stddev",
                 "type": np.float
             },
             "peak_error": {
-                "data": np.empty(self.shapes["photon_spacing"]),
+                "data": np.empty(self.shapes["stddev"]),
                 "path": "peak_error",
                 "type": np.float
             },
@@ -58,8 +71,6 @@ class ProcessXray(ProcessBase):
 
     def fit_peaks(self, hist, bins, npeaks, initial):
 
-        #print(bins)
-        #print(initial)
         bin_width = bins[1] - bins[0]
         window_bins = int(25/bin_width)
         fit_window = np.empty(npeaks, dtype='int')
@@ -69,9 +80,8 @@ class ProcessXray(ProcessBase):
         # loop over peaks, do fits
         for i in range(0, npeaks):
 
-            p_initial = [initial[0][i], initial[1][0], 10]
-            #print(np.where(bins==initial[0][i]))
-            fit_window[i] = np.where(bins==initial[0][i])[0]
+            p_initial = [initial[0][i], initial[1][i], 10]
+            fit_window[i] = np.where(bins==initial[1][i])[0]
 
             x_fit = bins[fit_window[i]-window_bins:fit_window[i]+window_bins]
 
@@ -85,11 +95,13 @@ class ProcessXray(ProcessBase):
         
 
     def get_photon_spacing(self, analog, row, col, mc):
-        #for one pixel
+        #for one pixel one memcell
 
-        #failed = (0, 0, (0, 0), (0, 0), 0)
-        #failed = (0, (0, 0), 0)
-        failed = 0
+        failed = ProcessXray.Res(photon_spacing=0, 
+                                 spacing_error=0, 
+                                 peak_stddev=(0,0), 
+                                 peak_error=(0,0), 
+                                 quality=0)
 
         bins = np.arange(np.min(analog), np.max(analog), 1, dtype='int16')
         (hist, _) = np.histogram(analog, bins)
@@ -108,6 +120,7 @@ class ProcessXray(ProcessBase):
         # find_peaks_cwt also finds many spurious peaks, filter these out
         # define minimum peak height
         min_height = 30
+        peak_loc_bins_filtered = peak_loc_bins[np.where(peak_sizes > min_height)]
         peak_sizes_filtered = peak_sizes[np.where(peak_sizes > min_height)]
         peak_locations_filtered = peak_locations[np.where(peak_sizes > min_height)]
 
@@ -123,28 +136,27 @@ class ProcessXray(ProcessBase):
             plt.gcf().clear()
             return failed
 
-        initial = [peak_locations_filtered, peak_sizes_filtered]
+        # fit 0- and 1-photon peaks with gaussian
+        initial = [peak_sizes_filtered, peak_locations_filtered]
+        params, pcov = self.fit_peaks(hist_smooth, bins, 2, initial)
+        results = list(zip(params[0], params[1]))
+        param_errors = [np.sqrt(np.diag(p)) for p in pcov]
+        errors = list(zip(param_errors[0], param_errors[1]))
 
-        # fit peaks with gaussian
-        params, pcov = self.fit_peaks(hist_smooth, bins, npeaks, initial)
-
-        fit_peak_locations = params[:][0]
-        fit_peak_sizes = params[:][1]
-                
-        photon_spacing = np.abs(fit_peak_locations[1] - fit_peak_locations[0])
-        #print(photon_spacing)
+        photon_spacing = np.abs(results[1][1] - results[1][0])
+        spacing_error = np.sqrt(np.power(errors[1][1], 2) + np.power(errors[1][0], 2))
+        fit_results = ProcessXray.Res
 
         if photon_spacing <= 10:
             return failed
 
-        #indices_between_peaks = np.sort(sorted_peak_indices[0:2])
-        #quality = sorted_peak_sizes[1] - np.min(hist_smooth[indices_between_peaks[0]:indices_between_peaks[1]])
-            
-        #peak_stddev = sorted_peak_stddev[0:2]
-            
-        #return (photon_spacing, spacing_error, peak_stddev, peak_errors, quality)
-        return (photon_spacing)
+        quality =  results[0][0] - np.min(hist_smooth[peak_loc_bins_filtered[0]:peak_loc_bins_filtered[1]])
 
+        return ProcessXray.Res(photon_spacing=photon_spacing,
+                               spacing_error=spacing_error,
+                               peak_stddev=results[2],
+                               peak_error=errors[2], 
+                               quality=quality)
 
 
     def calculate(self):
@@ -166,29 +178,29 @@ class ProcessXray(ProcessBase):
             
             failed_count = 0
             for row in range(self.n_rows):
-            #for row in range(1):
+            #for row in range(10):
                 print("row ", row)
 
+                #for col in range(64):
                 for col in range(self.n_cols):
 
                     for mc in range(self.n_memcells):
-                        #print("memcell {}".format(mc))
 
                         idx = (row, col, mc)
                         try:
-                            #(photon_spacing, spacing_error, peak_stddev, peak_errors, quality) = self.get_photon_spacing(analog)
-                            photon_spacing = self.get_photon_spacing(m_analog[row, col, mc, :], row, col, mc)
-                            self.result["photon_spacing"]["data"][idx] = photon_spacing
-                            #self.result["spacing_error"]["data"][idx] = spacing_error
-                            #self.result["peak_stddev"]["data"][idx] = peak_stddev
-                            #self.result["peak_error"]["data"][idx] = peak_error
-                            #self.result["quality"]["data"][idx] = quality
+                            fit_result = self.get_photon_spacing(m_analog[row, col, mc, :], row, col, mc)
+                            self.result["photon_spacing"]["data"][idx] = fit_result.photon_spacing
+                            self.result["spacing_error"]["data"][idx] = fit_result.spacing_error
+                            self.result["peak_stddev"]["data"][idx] = fit_result.peak_stddev
+                            self.result["peak_error"]["data"][idx] = fit_result.peak_error
+                            self.result["quality"]["data"][idx] = fit_result.quality
 
 
                         except:
                             print("memcell, row, col", mc, row, col)
                             print("analog.shape", analog.shape)
                             self.result["photon_spacing"]["data"][idx] = 0
+                            self.result["peak_stddev"]["data"][idx] = 0
                             raise
                             
                             
@@ -200,141 +212,3 @@ class ProcessXray(ProcessBase):
             print("Done.")
             total_fits = self.n_rows * self.n_cols * self.n_memcells
             print("Failed fits: ", failed_count, " = ", (failed_count/total_fits)*100, "%")
-
-
-    
-
-#######################################################################################
-# Yaroslav's functions
-#######################################################################################
-#
-#    def orderFilterCourse(self, data, window_radius, order, lowpass_points):
-#        if data.ndim != 1:
-#            raise ValueError('data must be one dimensional')
-#
-#        if window_radius%2 == 0:
-#            window_radius -= 1
-#
-#        sample_positions = np.round(np.linspace(window_radius, data.size - window_radius, lowpass_points)).astype(int)
-#        sample_values = np.zeros((sample_positions.size,))
-#        for i in np.arange(sample_positions.size):
-#            neighbourhood = data[(sample_positions[i] - window_radius):(sample_positions[i] + window_radius + 1)]
-#            sample_values[i] = np.sort(neighbourhood)[order]
-#
-#        lowpass_data = np.interp(np.arange(data.size), sample_positions, sample_values, left = sample_values[0], right = sample_values[-1])
-#        return lowpass_data
-#
-#
-#    def lowpass_correction(self, analog, locality_radius=800, lowpass_points=1000):
-#        data = analog
-#
-#        local_data = data[2 * locality_radius:6 * locality_radius]
-#        local_bin_edges = np.arange(np.min(local_data), np.max(local_data))
-#        
-#        (local_hist, _) = np.histogram(local_data, local_bin_edges)
-#        smooth_window_size = 11
-#        local_hist_smooth = convolve(local_histogram, np.ones((smooth_window_size,)), mode='same')
-#        
-#        most_frequent_value = np.mean(local_bin_edges[np.argmax(local_hist_smooth)]).astype(int)
-#        
-#        # set order to be the same order as the maximum of the histogram in the local data. Should be least affected by noise
-#        order = (2 * locality_radius * np.mean(np.nonzero(np.sort(local_data) == most_frequent_value)) / local_data.shape[0])
-#        
-#        if np.isnan(order):
-#            order = np.array(0.4 * 2 * locality_radius)
-#
-#        order = order.astype(int)
-#        lowpass_data = orderFilterCourse(data, locality_radius, order, lowpass_points)
-#        corrected_data = data - lowpass_data
-#        bin_edges = np.arange(np.min(corrected_data), np.max(corrected_data))
-#        
-#        return np.histogram(corrected_data, bin_edges)
-#
-#
-#    def indexes_peakutilsManuallyAdjusted(self, y, thres=0.3, min_dist=1):
-#        '''Peak detection routine.
-#        
-#        Finds the peaks in *y* by taking its first order difference. By using
-#        *thres* and *min_dist* parameters, it is possible to reduce the number of
-#        detected peaks. *y* must be signed.
-#        
-#        Parameters
-#        ----------
-#        y : ndarray (signed)
-#            1D amplitude data to search for peaks.
-#        thres : float between [0., 1.]
-#            Normalized threshold. Only the peaks with amplitude higher than the
-#            threshold will be detected.
-#        min_dist : int
-#            Minimum distance between each detected peak. The peak with the highest
-#            amplitude is preferred to satisfy this constraint.
-#            
-#        Returns
-#        -------
-#        ndarray
-#            Array containing the indexes of the peaks that were detected
-#            
-#        '''
-#        if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.unsignedinteger):
-#            raise ValueError("y must be signed")
-#
-#        thres = thres * (np.max(y) - np.min(y)) + np.min(y)
-#        min_dist = int(min_dist)
-#        
-#        # find the peaks by using the first order difference
-#        dy = np.diff(y)
-#        peaks = np.where((np.hstack([dy, 0.]) <= 0.)  # Addition by yaroslav.gevorkov@desy.de: instead of <, here <=
-#                         & (np.hstack([0., dy]) >= 0.)
-#                         & (y > thres))[0]
-#
-#        if peaks.size > 1 and min_dist > 1:
-#            highest = peaks[np.argsort(y[peaks])][::-1]
-#            rem = np.ones(y.size, dtype=bool)
-#            rem[peaks] = False
-#            
-#            for peak in highest:
-#                if not rem[peak]:
-#                    sl = slice(max(0, peak - min_dist), peak + min_dist + 1)
-#                    rem[sl] = True
-#                    rem[peak] = False
-#                    
-#            peaks = np.arange(y.size)[~rem]
-#
-#        return peaks
-#
-#
-#    def interpolate_peakutilsManuallyAdjusted(self, x, y, ind, width, func):
-#        '''Tries to enhance the resolution of the peak detection by using
-#        Gaussian fitting, centroid computation or an arbitrary function on the
-#        neighborhood of each previously detected peak index.
-#        
-#        Parameters
-#        ----------
-#        x : ndarray
-#            Data on the x dimension.
-#        y : ndarray
-#            Data on the y dimension.
-#        ind : ndarray
-#            Indexes of the previously detected peaks. If None, indexes() will be
-#            called with the default parameters.
-#        width : int
-#            Number of points (before and after) each peak index to pass to *func*
-#            in order to encrease the resolution in *x*.
-#        func : function(x,y)
-#            Function that will be called to detect an unique peak in the x,y data.
-#
-#        Returns
-#        -------
-#        ndarray :
-#            Array with the adjusted peak positions (in *x*)
-#        '''
-#
-#        out = []
-#        for slice_ in (slice(max((0,i - width)), min((x.size, i + width))) for i in ind):
-#            fit = func(x[slice_], y[slice_])
-#            out.append(fit)
-#            
-#        return np.array(out)
-#
-#
-########################################################################################
