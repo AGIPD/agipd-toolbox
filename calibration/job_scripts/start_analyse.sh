94916f4ca74eb9933ea0123d1cf656e6245e583e
Jennifer Poehlsen <jennifer.poehlsen@desy.de>
2020-01-16 16:39:05 +0100

    Updates due to changes in sbatch submission and maxwell environment

--- calibration/job_scripts/start_analyse.sh
+++ calibration/job_scripts/start_analyse.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-#source /etc/profile.d/modules.sh
+source /etc/profile.d/modules.sh
 #source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh
 #module load cfel-anaconda/py3-4.3.0
 
-source /etc/profile.d/modules.sh; source /gpfs/cfel/cxi/common/cfelsoft-rh7/setup.sh; module load cfel-python3/latest
+module load anaconda/3
 
 SCRIPT_DIR=$1
 shift
